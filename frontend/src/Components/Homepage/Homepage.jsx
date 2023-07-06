47516895c57290159267f41457e807faa5c7685a
ricardo-719 <teacherrickyricardo@gmail.com>
2023-07-06 16:33:58 +0700

    Implement fetch and render of recipe instances

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -1,20 +1,75 @@
 import './Homepage.css'
+import { useEffect, useState } from 'react'
 import {Header} from '../Header/Header'
 import Instances from '../Instances/Instances'
 
 const Homepage = () => {
+
+  const [recipes, setRecipes] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const endpointKeyOne = 'temp';
+  const endpointKeyTwo = 'temp';
+
+  //Fetch data
+  const fetchData = async(endpoint) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(endpoint);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        setRecipes(jsonResponse.recipes);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      setIsError(true);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${endpointKeyOne}${endpointKeyTwo}`)
+  }, [])
+
+  useEffect(() => {
+    if (recipes) {
+      setIsLoading(false)
+    } else {
+      setIsLoading(true)
+    }
+  }, [recipes])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  } 
+  else if (isError) {
+    return (
+      <section>
+        <h2>Failed to load...</h2>
+        {/* <button onClick={fetchData}>Try again</button> */}
+      </section>
+    )
+  }
+  else {
   return (
   <div id='homeComponent'>
     <header>
       <Header />
     </header>
     <section id='homeMain'>
-      <section id='recipesSocialSection'>
+     {/*  <section id='recipesSocialSection'>
         data
+      </section> */}
+      <section id='mainRecipesSection'>
+        {
+          (recipes != null) ? recipes.map((recipe) => <Instances key={recipe.id} recipe={recipe} />) : ''
+        }
       </section>
-      <Instances />
+      
     </section>
   </div>
   )
 }
+}
+
 export default Homepage
--- frontend/src/Components/Instances/Instances.jsx
+++ frontend/src/Components/Instances/Instances.jsx
@@ -1,21 +1,20 @@
 import './Instances.css'
 
-const Instances = () => {
-  return (
+const Instances = ({ recipe }) => {
+    
+    return (
     <ul class="flex flex-col p-2 ml-2">
-        <h2>Recipes</h2>
         {/* Add Loop Here */}
             <div class="cursor-default" href="">
                 <div class="flex flex-col md:flex-row md:max-w-2xl rounded-lg bg-grey shadow-lg p-4">
                     {/* If Recipe Cotains Image */}
                     {/* <img src="" alt="" hidden="true"> */}
                     {/* Else */}
-                    {/* <img src="{{ exercise.exerciseImg }}" alt="Exercise Image" 
-                    class="w-full max-w-xs h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"> */}
+                    <img src={recipe.image} alt="recipe Image" class="w-full max-w-xs h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" />
                     {/* End If */}
                     <div class="p-6 flex flex-col justify-start">
                         <li>
-                            <h2 class="inline-block text-gray-900 text-xl font-medium mb-2">Recipe Name</h2>
+                            <h2 class="inline-block text-gray-900 text-xl font-medium mb-2">{recipe.title}</h2>
                             <span id="" class="float-right flex">
                                 <i class="fa-regular fa-square-plus pl-2 hover:text-blue-600 cursor-pointer add-exercise" title="Add"></i>
                             </span>
