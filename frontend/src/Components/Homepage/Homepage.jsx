64fc5d2636af994595e07f7e9b748e7fb1f83357
ricardo-719 <teacherrickyricardo@gmail.com>
2023-07-07 10:21:46 +0700

    Update recipes section and set up logic for recipes details

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -6,10 +6,11 @@ import Instances from '../Instances/Instances'
 const Homepage = () => {
 
   const [recipes, setRecipes] = useState("");
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const endpointKeyOne = 'placeholder';
-  const endpointKeyTwo = 'placeholder';
+  const endpointKeyTwo = '6d6bc2a3b1904ff5cd3';
 
   //Fetch data
   const fetchData = async(endpoint) => {
@@ -39,6 +40,14 @@ const Homepage = () => {
     }
   }, [recipes])
 
+  const tumbler = () => {
+    fetchData(`https://api.spoonacular.com/recipes/random?number=4&apiKey=${endpointKeyOne}${endpointKeyTwo}`)
+  }
+
+  const handleRecipeClick = (recipe) => {
+    setSelectedRecipe(recipe);
+  }
+
   if (isLoading) {
     return (
     <div id='homeComponent'>
@@ -49,10 +58,10 @@ const Homepage = () => {
         <section id='recipesRandomizer'>
           <h3>Unsure what to eat?</h3>
           <h4>Randomize your next meal:</h4>
-          <i class="fa-solid fa-recycle iconRandomizer cursor-pointer"></i>
+          <i className="fa-solid fa-recycle iconRandomizer cursor-pointer" onClick={() => {tumbler()}}></i>
         </section>
         <section id='mainRecipesSection'>
-          <h2>Loading...</h2>
+          <h3 className='md:max-w-2xl md:w-2xl'>Loading...</h3>
         </section>
       </section>
     </div>
@@ -68,11 +77,31 @@ const Homepage = () => {
         <section id='recipesRandomizer'>
           <h3>Unsure what to eat?</h3>
           <h4>Randomize your next meal:</h4>
-          <i class="fa-solid fa-recycle iconRandomizer cursor-pointer"></i>
+          <i className="fa-solid fa-recycle iconRandomizer cursor-pointer" onClick={() => {tumbler()}}></i>
         </section>
         <section id='mainRecipesSection'>
           <h2>Failed to load...</h2>
           {/* <button onClick={fetchData}>Try again</button> */}
+        </section>
+      </section>
+    </div>
+    )
+  }
+  else if (selectedRecipe) {
+    return (
+    <div id='homeComponent'>
+      <header>
+        <Header />
+      </header>
+      <section id='homeMain'>
+        <section id='recipesRandomizer'>
+          <h3>Unsure what to eat?</h3>
+          <h4>Randomize your next meal:</h4>
+          <i className="fa-solid fa-recycle iconRandomizer cursor-pointer" onClick={() => {tumbler()}}></i>
+        </section>
+        <section id='mainRecipesSection'>
+          I AM WORKING!!
+          <Instances recipe={selectedRecipe} />
         </section>
       </section>
     </div>
@@ -88,11 +117,11 @@ const Homepage = () => {
       <section id='recipesRandomizer'>
         <h3>Unsure what to eat?</h3>
         <h4 className='subTitle'>Randomize your next meal:</h4>
-        <i className="fa-solid fa-recycle iconRandomizer cursor-pointer"></i>
+        <i className="fa-solid fa-recycle iconRandomizer cursor-pointer" onClick={() => {tumbler()}}></i>
       </section>
       <section id='mainRecipesSection'>
         {
-          (recipes != null) ? recipes.map((recipe) => <Instances key={recipe.id} recipe={recipe} />) : ''
+          (recipes != null) ? recipes.map((recipe) => <Instances key={recipe.id} recipe={recipe} onClick={() => handleRecipeClick(recipe)} />) : ''
         }
       </section>
     </section>
--- frontend/src/Components/Instances/Instances.jsx
+++ frontend/src/Components/Instances/Instances.jsx
@@ -1,30 +1,35 @@
 import './Instances.css'
 
-const Instances = ({ recipe }) => {
+const Instances = ({ recipe, onClick }) => {
+
+    const maxCharacters = 100;
+    let recipeDescription = recipe.summary;
+
+    if (recipeDescription.length > maxCharacters) {
+        recipeDescription = recipeDescription.slice(0, maxCharacters) + '...';
+    }
+
+    const createMarkup = () => {
+    return { __html: recipeDescription };
+    };
     
     return (
-    <ul class="flex flex-col p-2 ml-2">
-        {/* Add Loop Here */}
-            <div class="cursor-default" href="">
-                <div class="flex flex-col md:flex-row md:max-w-2xl rounded-lg bg-grey shadow-lg p-4">
-                    {/* If Recipe Cotains Image */}
-                    {/* <img src="" alt="" hidden="true"> */}
-                    {/* Else */}
-                    <img src={recipe.image} alt="recipe Image" class="w-full max-w-xs h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" />
-                    {/* End If */}
-                    <div class="p-6 flex flex-col justify-start">
+    <ul className="flex flex-col p-2 ml-2">
+            <div className="cursor-pointer hover:opacity-60" href="" onClick={onClick}>
+                <div className="flex flex-col md:flex-row md:max-w-2xl rounded-lg bg-grey shadow-lg p-4">
+                    <img src={recipe.image} alt="recipe" className="w-full max-w-xs h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" />
+                    <div className="p-6 flex flex-col justify-start">
                         <li>
-                            <h2 class="inline-block text-gray-900 text-xl font-medium mb-2">{recipe.title}</h2>
-                            <span id="" class="float-right flex">
-                                <i class="fa-regular fa-square-plus pl-2 hover:text-blue-600 cursor-pointer add-exercise" title="Add"></i>
-                            </span>
+                            <h2 className="inline-block text-gray-900 text-xl font-medium mb-2">{recipe.title}</h2>
+                            {/* <span id="" className="float-right flex">
+                                <i className="fa-regular fa-square-plus pl-2 hover:text-blue-600 cursor-pointer add-exercise" title="Add"></i>
+                            </span> */}
                         </li>
-                        <p class="text-gray-700 text-base mb-4"><i>Recipe Description</i></p>
-                        <p class="text-gray-600 text-xs">Recipe Category</p>
+                        <p className="text-gray-700 text-base mb-4" dangerouslySetInnerHTML={createMarkup()}></p>
+                        <p className="text-gray-600 text-xs"><b>Ready in:</b> {recipe.readyInMinutes} minutes</p>
                     </div>
                 </div>
             </div>
-        {/* End Loop Here */}
     </ul>
   )
 }
